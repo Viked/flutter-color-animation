cb699fde02e0731c05e851b6555ba0c12ac70b61
Yevgenii Shein <initbin@gmail.com>
2020-02-11 23:18:22 +0200

    Add color animation

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,62 +12,62 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   final rng = Random();
 
-  Color _randomColor() =>
-      Color.fromARGB(255, rng.nextInt(255), rng.nextInt(255), rng.nextInt(255));
-
-  Color _color;
+  AnimationController _animationController;
+  ColorTween _colorTween;
+  Animation _colorTweenAnimation;
 
   @override
   void initState() {
+    final color = _randomColor();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _colorTween = ColorTween(begin: color, end: color);
+    _colorTweenAnimation = _colorTween.animate(_animationController);
     super.initState();
-    _color = _randomColor();
   }
 
-  void _setColor() {
-    setState(() {
-      _color = _randomColor();
-    });
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: GestureDetector(
-          child: Container(
-            color: _color,
-            child: Center(
-              child: Text(
-                'Hey there',
+            child: AnimatedBuilder(
+              animation: _colorTweenAnimation,
+              builder: (context, child) => Container(
+                color: _colorTweenAnimation.value,
+                child: child,
               ),
+              child: Center(child: Text('Hey there')),
             ),
-          ),
-          onTap: _setColor,
-        ));
+            onTap: _setColor));
+  }
+
+  Color _randomColor() =>
+      Color.fromARGB(255, rng.nextInt(255), rng.nextInt(255), rng.nextInt(255));
+
+  void _setColor() {
+    _colorTween.begin = _colorTweenAnimation.value;
+    _colorTween.end = _randomColor();
+    _animationController.reset();
+    _animationController.forward();
   }
 }
